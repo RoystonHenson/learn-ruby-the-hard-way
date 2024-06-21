eb48e55a2ccab9c15c4ffd2744b448feb55c93d6
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-21 19:58:25 +0100

    Refactor Ex1

--- ex1.rb
+++ ex1.rb
@@ -1,9 +1,11 @@
-=beginputs 'Hello World!'
-puts 'Hello Again'
-puts 'I like typing this.'
-puts 'This is fun'
-puts 'Yay! Printing.'
-puts "I'd much rather you 'not'"
-puts "I 'said' do not touch this."
+=begin
+puts "  Hello World!
+  Hello Again.
+  I like typing this.
+  This is fun.
+  Yay! Printing.
+  I'd much rather you 'not'.
+  I 'said' do not touch this.
+"
 =end
-puts 'Here is another line!'
+puts '  Here is another line!'
