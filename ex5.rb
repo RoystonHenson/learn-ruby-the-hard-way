a86bffea46bde49832c51cbf73995d423cff5fce
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-21 19:59:25 +0100

    Remove unnecessary comments

--- ex5.rb
+++ ex5.rb
@@ -1,8 +1,8 @@
 name = 'Zed A. Shaw'
-age = 35 # not a lie in 2009
-height_in_inches = 74 # inches
+age = 35 
+height_in_inches = 74 
 height_in_cm = height_in_inches * 2.54
-weight_in_lbs = 180 # lbs
+weight_in_lbs = 180 
 weight_in_kg = 180 * 0.453592
 eyes = 'Blue'
 teeth = 'White'
@@ -15,6 +15,5 @@ print "  Let's talk about #{name}.
   He's got #{eyes.downcase} eyes and #{hair.downcase} hair.
   His teeth are usually #{teeth.downcase} depending on the coffee.
 "
-  # this line is tricky, try to get it exactly right
   puts "  If I add #{age}, #{height_in_inches}, and #{weight_in_lbs} I get #{age + height_in_inches + weight_in_lbs}."
 
