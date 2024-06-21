4f42087d347391a0e62819e3b90c71a53436b010
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-21 13:48:14 +0100

    Complete Ex3

---
+++ ex3.rb
@@ -0,0 +1,31 @@
+# Display string
+puts 'I will now count my chickens:'
+
+# Evaluate expression and output number of hens/roosters
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Display string
+puts 'Now I will count the eggs:'
+
+# Evaluate and display expression
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 -1.0 / 4.0 + 6.0
+
+# Display string
+puts 'Is it true that 3 + 2 < 5 - 7?'
+
+# Evaluate and display expression
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# Evaluate expresssions and display strings
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# Display strings
+puts "Oh, that's why it's false"
+puts 'How about some more.'
+
+# Evaluate booleans and display strings
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{ 5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
