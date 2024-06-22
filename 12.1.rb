7263753fa6ff7c96429ebb6e91aad757172b95a1
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-22 16:51:14 +0100

    Remove misnamed file

--- 12.1.rb
+++
@@ -1,4 +0,0 @@
-print "Hi there! Give me some money and I'll show you a magic trick! Type in how much you give me: "
-amount = gets.chomp.to_f
-money_back = amount * 0.1
-print "Hey presto! Here is #{money_back.round(2)} back! The rest of it I made dissapear. Impressive right?"
