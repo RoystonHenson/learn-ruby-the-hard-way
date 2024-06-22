431570ba583b3475369597df240458a95f25ca5f
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-22 16:17:37 +0100

    Complete Ex 11

---
+++ ex11.1.rb
@@ -0,0 +1,8 @@
+print 'Welcome to our kebab shop! What meat would you like? '
+meat = gets.chomp
+print 'What salad you want with that? '
+salad = gets.chomp
+print 'Any sauce? '
+sauce = gets.chomp
+
+print "Ok great! Just to check, you want a #{meat} kebab with #{salad} and #{sauce} sauce. Coming right up!"
