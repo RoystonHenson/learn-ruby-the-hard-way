e69e3ea0f28e65d4594d9ab245bd74a3cdbfba16
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-24 14:20:57 +0100

    Refactor Ex30 to use one-line ifs

--- ex21-30/ex29.rb
+++ ex21-30/ex29.rb
@@ -2,32 +2,18 @@ people = 20
 cats = 30
 dogs = 15
 
-if people < cats
-    puts 'Too many cats! The world is doomed!'
-end
+puts 'Too many cats! The world is doomed!' if people < cats
 
-if people > cats
-    puts 'Not many cats! The world is saved!'
-end
+puts 'Not many cats! The world is saved!'if people > cats
+    
+puts 'The world is drooled on!' if people < dogs
 
-if people < dogs
-    puts 'The world is drooled on!'
-end
-
-if people > dogs
-    puts 'The world is dry!'
-end
+puts 'The world is dry!' if people > dogs
 
 dogs += 5
 
-if people >= dogs
-    puts 'People are greater that or equal to dogs.'
-end
+puts 'People are greater that or equal to dogs.' if people >= dogs
 
-if people <= dogs
-    puts 'People are less than or equal to dogs.'
-end
+puts 'People are less than or equal to dogs.' if people <= dogs
 
-if people == dogs
-    puts 'People are dogs.'
-end
+puts 'People are dogs.' if people == dogs
