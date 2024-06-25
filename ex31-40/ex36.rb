7415d9f9dd4024d5e838b8dd57817f1d2349f7c7
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-25 12:19:14 +0100

    Refactor 'starting room' in Ex36

--- ex31-40/ex36.rb
+++ ex31-40/ex36.rb
@@ -1,11 +1,22 @@
-def prompt; print "->> "; end
+def prompt
+    print "->> "
+end
+
+def get_choice
+    prompt
+    @choice = $stdin.gets.chomp.downcase
+end
 
 def you_lose(event)
     puts "\n\t<***> #{event}\n\t      Ouch! Game over... <***>"
     exit(0)
 end
 
-=begin
+def you_win
+    puts 'What a legend! RAHHH!'
+    exit(0)
+end
+
 def starting_room
     puts "Announcer:\n\t\"Welome to the challenge rooms!\n\t Your task is to escape here alive!\n\t Good luck!\""
     puts "\n\tBefore you are two doors;\n\ta pristine-looking door on the left\n\tand a shoddy one on the right."
@@ -13,25 +24,21 @@ def starting_room
     left_door_closed = true
     while true
         puts "\nDo you choose to approach the door on the left or the right?"
-        prompt
-        choice = $stdin.gets.chomp
-     
-        if choice.include?('left') && left_door_closed == true
+        get_choice
+        if @choice.include?('left') && left_door_closed == true
             puts "\n\t*You open the pristine door only to find yourself staring at a wall.\n\t There is no way through.*"
             left_door_closed = false
-        elsif choice.include?('left') && left_door_closed == false
-            puts "\n\tYou have already opened this door! Don't you remember?"    
-        elsif choice.include?('right')
+        elsif @choice.include?('left') && left_door_closed == false
+            puts "\n\tYou have already opened this door silly! Don't you remember?"    
+        elsif @choice.include?('right')
             while true
                 puts "\n\t*You approach the shoddy door on the right.*\n\nAre you sure you want to open it?"
-                choice = $stdin.gets.chomp
-                    
-                if choice.include?('right') || choice.include?('yes')
+                get_choice
+                if @choice.include?('right') || @choice.include?('yes')
                     puts "\n\t*You attempt to open the door but with difficulty. You try again\n\t and it falls off its hinges revealing a pasageway. You move through\n\t cautiously and find yourself in another narrow room.*"
                     trap_door_room
                 else
-                    puts "\n\t*You wander around the room aimlessly until you pass out*"  # change later to use you_lose
-                    exit(0)
+                    you_lose('You wander around the room aimlessly until you pass out.')
                 end
             end
         else    
@@ -39,10 +46,10 @@ def starting_room
         end
     end
 end
-=end
+
 def trap_door_room
-    puts "\n\tSpanning the room are nine large tiles arranged three by three. They each\n\thave one letter on them, reading from left to right: L, F, R."
-    puts "\tBeyond the tiles you can see another door."
+    puts "\n\t*Spanning the room are nine large tiles arranged three by three. They each\n\t have one letter on them, reading from left to right: L, F, R."
+    puts "\t Beyond the tiles you can see another door."
     
     while true
         puts "\nWhich tile do you step on?: 'L' 'F' or 'R'?"
@@ -58,8 +65,8 @@ def trap_door_room
                         puts "\nOnly one set of tiles remains! From here you can step onto 'L', 'F' or 'R'.\nWhat do you choose?"
                         get_choice
                         if @choice == 'r'
-                            puts 'you made it!'
-                            exit(0)
+                            puts "\n\t*With a huge sign of relief, you step off the tiles. You made it!\n\t You open the door and enter the next room.*"
+                            swinging_axe_room
                         else
                             you_lose("The tile explodes sending you flying into the air and back\n\t      down into the gaping maw below!")
                         end
@@ -74,19 +81,11 @@ def trap_door_room
     end
 end
 
-def get_choice
-    prompt
-    @choice = $stdin.gets.chomp.downcase
-end
-
-# trap door room
-
-# swining axe room
+# swinging axe room
 
 # ravine
 
 # final room
 
 # delete to run code
-#starting_room
-trap_door_room
+starting_room
